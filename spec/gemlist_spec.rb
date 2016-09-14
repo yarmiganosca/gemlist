834fce01649218749eb60032aa65acb40f6b8f47
Chris Hoffman <choffman@optoro.com>
2016-09-14 11:56:27 -0400

    use the gems the test project actually installs

--- spec/gemlist_spec.rb
+++ spec/gemlist_spec.rb
@@ -3,7 +3,7 @@ describe Gemlist do
     gemlist = Gemlist.new(test_project, without: [:development, :test])
     names   = gemlist.gems.map(&:name)
 
-    expect(names).to eq %w(hammer)
+    expect(names).to eq %w(gemlist vise progress thor mime-types netrc unf_ext unf domain_name http-cookie rest-client anvil-cli hammer)
   end
 
   it "blows up if given conflicting group lists" do
