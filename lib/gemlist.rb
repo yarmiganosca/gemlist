481d0aa141dcf4a1d18c4577d76d5ae3c65816c3
Chris Hoffman <choffman@optoro.com>
2016-09-14 11:57:45 -0400

    it works now

--- lib/gemlist.rb
+++ lib/gemlist.rb
@@ -1,6 +1,7 @@
 require "pathname"
 require "gemlist/version"
 require "gemlist/options"
+require "gemlist/spec_tree"
 
 class Gemlist
   def initialize(path, options = {})
@@ -9,13 +10,46 @@ class Gemlist
   end
 
   def gems
-    []
+    specs.each.with_index.with_object([]) do |(spec, index), uniquified_specs|
+      last_occurrence_of_spec = specs.rindex(spec) == index
+
+      uniquified_specs << spec if last_occurrence_of_spec
+    end
   end
 
   private
 
+  def specs
+    spec_tree = SpecTree.new(lockfile)
+
+    spec_tree
+      .depth_first_children_first
+      .reject(&:is_root?)
+      .select { |node| node.descends_from?(*top_level_dependencies) }
+      .map(&:content)
+  end
+
+  def top_level_dependencies
+    bundle_definition
+      .dependencies
+      .select { |dependency| (dependency.groups & groups).any? }
+  end
+
+  def bundle_definition
+    return @bundle_definition unless @bundle_definition.nil?
+
+    builder = Bundler::Dsl.new
+    builder.eval_gemfile(path/"Gemfile")
+
+    @bundle_definition = builder.to_definition(lockfile, {})
+  end
+
+  def lockfile
+    @lockfile ||= path/"Gemfile.lock"
+  end
+
   def groups
-    ([:default] + options.included_groups - options.excluded_groups).uniq
+    bundle_definition.groups + options.included_groups - options.excluded_groups
   end
 
   attr_reader :path, :options
---
+++ lib/gemlist/spec_tree.rb
@@ -0,0 +1,72 @@
+require 'tree'
+require 'bundler'
+
+class Gemlist
+  class SpecNode < Tree::TreeNode
+    def total_dependency_count
+      size - 1
+    end
+
+    def depth_first_children_first
+      sorted_children = children.sort_by(&:total_dependency_count)
+      parent          = self
+
+      Enumerator.new do |yielder|
+        sorted_children.each do |child|
+          child.depth_first_children_first.each do |node|
+            yielder << node
+          end
+        end
+
+        yielder << parent
+      end
+    end
+
+    def lineage
+      [self, *parentage].reverse.reject(&:is_root?)
+    end
+
+    def descends_from?(*dependencies)
+      !is_root? &&
+        dependencies.map(&:name).include?(lineage.first.name)
+    end
+  end
+
+  class SpecTree < SpecNode
+    def initialize(lockfile)
+      lockfile_parser = Bundler::LockfileParser.new(lockfile.read)
+
+      super("root", lockfile_parser)
+
+      specs_to_place = lockfile_parser.specs.to_set
+
+      # start with the top-level declared dependencies
+      lockfile_parser.dependencies.each do |dependency|
+        spec_for_dependency = specs_to_place.find { |spec| spec.name == dependency.name }
+
+        self << SpecNode.new(spec_for_dependency.name, spec_for_dependency)
+
+        specs_to_place.delete(spec_for_dependency)
+      end
+
+      until specs_to_place.empty?
+        specs_to_place.each do |spec|
+          # we find all the specs that are parent to this one, so that we can
+          # just remove entire top-level branches when it comes time to exclude
+          # groups
+          parent_nodes = select do |node|
+            node.content.dependencies.any? do |dependency|
+              dependency.name == spec.name
+            end
+          end
+
+          parent_nodes.each do |node|
+            node << SpecNode.new(spec.name, spec)
+          end
+
+          specs_to_place.delete(spec) if parent_nodes.any?
+        end
+      end
+    end
+  end
+end
